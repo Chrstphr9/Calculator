eb44a6055deae7787d6767812cceeee4b7f003f6
Chrstphr <somto565@gmail.com>
2023-07-07 14:45:45 +0200

    update

--- main.js
+++ main.js
@@ -8,37 +8,65 @@ for (let key of keys) {
   const value = key.dataset.key;
 
   key.addEventListener("click", () => {
-    if (value == "clear") {
+    if (value === "clear") {
       input = "";
       display_input.innerHTML = "";
       display_output.innerHTML = "";
-    } else if (value == "backspace") {
+    } else if (value === "backspace") {
       input = input.slice(0, -1);
-      display_input.innerHTML = "input";
-    } else if (value == "=") {
+      display_input.innerHTML = cleanInput(input);
+    } else if (value === "=") {
       let result = eval(input);
-
-      display.display_output.innerHTML = result;
-    } else if (value == "brackets") {
+      try {
+        result = new Function('return ' + input)();
+        display_output.innerHTML = result;
+      } catch (error) {
+        display_output.innerHTML = 'Error';
+      }
+    } else if (value === "brackets") {
       if (
-        input.indexOf("(") == -1 ||
-        (input.indexOf("(") != -1 &&
-          input.indexOf(")") != -1 &&
+        input.indexOf("(") === -1 ||
+        (input.indexOf("(") !== -1 &&
+          input.indexOf(")") !== -1 &&
           input.lastIndexOf("(") < input.lastIndexOf(")"))
       ) {
         input += "(";
       } else if (
-        (input.indexOf("(") != -1 && input.indexOf(")") == -1) ||
-        (input.indexOf("(") != -1 &&
-          input.indexOf(")") != -1 &&
+        (input.indexOf("(") !== -1 && input.indexOf(")") === -1) ||
+        (input.indexOf("(") !== -1 &&
+          input.indexOf(")") !== -1 &&
           input.lastIndexOf("(") < input.lastIndexOf(")"))
       ) {
-        input -= ";";
+        input += ")";
       }
-      display_input.innerHTML = input;
+      display_input.innerHTML = cleanInput(input);
     } else {
-      input += value; 
-      display_input.innerHTML = input;
+      input += value;
+      display_input.innerHTML = cleanInput(input);
     }
   });
 }
+
+function cleanInput(input) {
+    let input_array = input.split("");
+    let input_array_length = input_array.length;
+  
+    for (let i = 0; i < input_array_length; i++) {
+      if (input_array[i] === "/") {
+        input_array[i] = `<span class='operator'>/</span>`;
+      } else if (input_array[i] === "+") {
+        input_array[i] = `<span class='operator'>+</span>`;
+      } else if (input_array[i] === "-") {
+        input_array[i] = `<span class='operator'>-</span>`;
+      } else if (input_array[i] === "(") {
+        input_array[i] = `<span class='brackets'>(</span>`;
+      } else if (input_array[i] === ")") {
+        input_array[i] = `<span class='brackets'>)</span>`;
+      } else if (input_array[i] === "%") {
+        input_array[i] = `<span class='operator'>%</span>`;
+      }
+    }
+  
+    return input_array.join("");
+  }
+  
